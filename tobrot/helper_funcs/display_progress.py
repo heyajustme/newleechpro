e6c52da466f372056db4cca58bac873a7de83489
heyajustme <84226002+heyajustme@users.noreply.github.com>
2021-09-25 12:43:56 +0530

    Update display_progress.py

--- tobrot/helper_funcs/display_progress.py
+++ tobrot/helper_funcs/display_progress.py
@@ -88,7 +88,7 @@ class Progress:
                 humanbytes(speed),
                 # elapsed_time if elapsed_time != '' else "0 s",
                 estimated_total_time if estimated_total_time != "" else "0 s",
-            #tmp += "\n│"+"\n╰── ⌊ @TGFilmZone ⌉"
+            #tmp += "\n│"+"\n╰── ⌊ IU💞 ⌉"
             )
             try:
                 if not self._mess.photo:
